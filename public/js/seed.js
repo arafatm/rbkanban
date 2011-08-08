a077688ca413c1ae1f39525d14b6774e8e83e4f9
Arafat Mohamed <github@arafatm.com>
2011-08-08 16:51:16 -0500

    Removed absolute url in ajax load

--- public/js/seed.js
+++ public/js/seed.js
@@ -1,5 +1,5 @@
 //$.getJSON('data.json', function(data) {
-$.getJSON("http://localhost:9393/features", function(data) {
+$.getJSON("/features", function(data) {
   $.each(data, function(fk, fv) {
     var f = new Feature(fv.id, fv.title, fv.status, fv.state);
     $.each(fv.comments, function(ck, cv) {
