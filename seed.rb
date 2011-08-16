cae0793dba83eb7c914cf03283e88a855b6351bf
Arafat Mohamed <github@arafatm.com>
2011-08-16 10:14:47 -0500

    Updating seed file to generate more realistic datestamps for comments

--- seed.rb
+++ seed.rb
@@ -8,12 +8,18 @@ end
 statuses = ["Backlog", "Analysis", "Dev", "Verify", "Release"]
 states = ["Ready", "Progress", "Impeded"]
 10.times do |i|
+  t1 = Time.now - (rand(60*60*24*30)) 
   f = Feature.new(:title => "Feature #{i}",
                   :status => statuses[rand(statuses.size)],
                   :state => states[rand(states.size)],
                   :complete => false)
   rand(5).to_i.times do |j|
-    f.comments << Comment.new(:comment => "Comment #{j}")
+    t = t1 + rand(60*60*24*30) 
+    while t > Time.now
+      t = t1 + rand(60*60*24*30) 
+    end
+    t1 = t
+    f.comments << Comment.new(:comment => "Comment #{j}", :created_at => t1)
   end
   f.save
 end
